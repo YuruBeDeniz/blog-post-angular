fc110158cac2a1ef84636e5c6051f77a980337e0
YuruBeDeniz <saganak.deniz@gmail.com>
2024-12-30 14:35:03 +0100

    feat:scaffold the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { TopicsComponent } from './components/topics/topics.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { TopicDetailComponent } from './components/topic-detail/topic-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, 
+    { path: 'topics', component: TopicsComponent }, 
+    { path: 'posts', component: PostsComponent },
+    { path: 'posts/:postId', component: PostDetailComponent }, 
+    { path: 'topics/:topicId', component: TopicDetailComponent }, 
+    { path: 'signup', component: SignupComponent }, 
+    { path: 'login', component:  LoginComponent}, 
+];
---
+++ src/app/models/blog-post.model.ts
@@ -0,0 +1,7 @@
+export type BlogPost = {
+    id?: string;
+    post: string;
+    author: string;
+    topic: string;
+    created_at: string;
+}
